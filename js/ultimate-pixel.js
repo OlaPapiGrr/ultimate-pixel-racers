1de5ebd64760ae9cc01181f61783ec66a7a2ce90
BryanOlaPapiGrr <bryan.amrein@gmx.ch>
2021-12-15 15:13:59 +0100

    collisions

--- js/ultimate-pixel.js
+++ js/ultimate-pixel.js
@@ -1,38 +1,53 @@
-var x = 50;
-var y = 50;
-var rotZ = 0;
+var x = 455;
+var y = 65;
+var midX;
+var midY;
+var rotZ = 90;
 var acc = 0;
+var revacc = 0;
+var background;
+var car1;
+var rect1;
 
+
+function preload(){
+  background = loadImage("pictures/Racetreck1.png");
+  car1 = loadImage("pictures/Car1.png");
+}
 
 function setup() { 
-    alert("setup");
-  createCanvas(400, 400);
-  angleMode(DEGREES)
-  rectMode(CENTER);
-   
+
+  createCanvas(1265, 555);
+  angleMode(DEGREES);
 } 
 
 function draw() { 
-  background(220);
-  fill(0);
-
-  console.log(millis());
   
+  image(background, 0, 0);
+  
+  verlangsameAutoAusserhalb();
+  wandKollision();
+  outOfBounce();
   steuereAuto();
   
-  rect(0,0,30,50);
- 
+  image(car1,0,0);
+  
+  
+  console.log("Y ",mouseY);
+  console.log("X ",mouseX);
+  console.log("mY ",midY);
+  console.log("mX ",midX);
 }
 
 function steuereAuto() {
   
   if (keyIsDown(UP_ARROW)) {
     if (keyIsDown(LEFT_ARROW)) {
-    rotZ = rotZ - 4;
-    } else if (keyIsDown(RIGHT_ARROW)) {
-    rotZ = rotZ + 4;
-  }
-    if (millis() % 2== 0 && acc < 7){
+      rotZ = rotZ - 3;
+      } else if (keyIsDown(RIGHT_ARROW)) {
+      rotZ = rotZ + 3;
+    }
+    if (millis() % 2== 0 && acc < 2){
         acc += 0.5;
     }
     y += cos(rotZ)*acc;
@@ -40,21 +55,101 @@ function steuereAuto() {
   } else if (keyIsDown(DOWN_ARROW)) {
     
     if (keyIsDown(RIGHT_ARROW)) {
-    rotZ = rotZ + 2;
+    rotZ = rotZ + 3;
   } else if (keyIsDown(LEFT_ARROW)) {
-    rotZ = rotZ - 2;
+    rotZ = rotZ - 3;
   }
-    y -= cos(rotZ)*1;
-    x += sin(rotZ)*1;
+  if (millis() % 2== 0 && revacc < 1){
+    revacc += 0.5;
+  }
+    y -= cos(rotZ)*revacc;
+    x += sin(rotZ)*revacc;
   } else {
-     acc = 0;
+     while(acc != 0){
+      if (millis() % 2== 0){
+        tempacc = acc;
+        tempacc -= 0.5;
+        acc = tempacc;
+      }
+     }
     }
-  
-  
-  
-  
   translate(x,y);
   rotate(rotZ);
+
+ 
+}
+
+
+function verlangsameAutoAusserhalb(){
+  midX = x + 13;
+  midY = y - 22.5;
+  midXtemp = midX;
+  midYtemp = midY;
+  
+  if(midXtemp > 0 && midXtemp < 70 && midYtemp > 0 && midYtemp < 555){
+    acc = 1;
+  } else if(midXtemp > 0 && midXtemp < 1210 && midYtemp > 0 && midYtemp < 8){
+    acc = 1;
+  } else if(midXtemp > 930 && midXtemp < 1210 && midYtemp > 0 && midYtemp < 555){
+    acc = 1;
+  } else if(midXtemp > 0 && midXtemp < 1210 && midYtemp > 510 && midYtemp < 555){
+    acc = 1;
+  } else if(midXtemp > 400 && midXtemp < 550 && midYtemp > 330 && midYtemp < 555) {
+    acc = 1;
+  }
+}
+
+
+
+function wandKollision(){
+  midX = x + 13;
+  midY = y - 22.5;
+  midXtemp = midX;
+  midYtemp = midY;
+ 
+  if(midXtemp > 185 && midXtemp < 800 && midYtemp > 100 && midYtemp < 200){
+    acc = 0;
+    y -= cos(rotZ)*2;
+    x += sin(rotZ)*2;
+  } 
+  if(midXtemp > 180 && midXtemp < 285 && midYtemp > 100 && midYtemp < 420){
+    acc = 0;
+    y -= cos(rotZ)*2;
+    x += sin(rotZ)*2;
+  }
+  if(midXtemp > 650 && midXtemp < 805 && midYtemp > 200 && midYtemp < 420){
+    acc = 0;
+    y -= cos(rotZ)*2;
+    x += sin(rotZ)*2;
+  }
+}
+
+function outOfBounce(){
+  midX = x + 13;
+  midY = y - 22.5;
+  midXtemp = midX;
+  midYtemp = midY;
+
+  if(midXtemp > 0 && midXtemp < 1210 && midYtemp > 0 && midYtemp < 2){
+    x = 370
+    y = 65
+    rotZ = 90
+  } 
+  if(midXtemp > 0 && midXtemp < 2 && midYtemp > 0 && midYtemp < 555){
+    x = 370
+    y = 65
+    rotZ = 90
+  } 
+  if(midXtemp > 0 && midXtemp < 1210 && midYtemp > 554 && midYtemp < 555){
+    x = 370
+    y = 65
+    rotZ = 90
+  } 
+  if(midXtemp > 1015 && midXtemp < 1210 && midYtemp > 0 && midYtemp < 555){
+    x = 370
+    y = 65
+    rotZ = 90
+  } 
 }
 
 
